39d10a16f37b35dd82ab631d2f157200831334d0
Dovi Salomon <Dovisal1@gmail.com>
2014-12-06 17:37:00 -0500

    commented the cpp file

--- fraction.cpp
+++ fraction.cpp
@@ -9,8 +9,12 @@
 
 using namespace std;
 
+//Default constructor. No initilization.
 Fraction::Fraction(void) { }
 
+//The fraction will be intialized with the first parameter as the numerator
+//and the second parameter as the denominator.
+//The fraction will automatically be reduced
 Fraction::Fraction(int u, int v){
     num = u;
     den = v;
@@ -23,8 +27,11 @@ void Fraction::setValues(int u, int v) {
     this -> reduce();
 }
 
+//Reduces the fraction into its simplest form.
+//First it finds the greatest common factor and then devides both
+//the numerator and denominator by that value.
 void Fraction::reduce(void) {
-    int u = num, v=den, temp;
+    int u = num, v = den, temp;
     
     while (v != 0) {
         temp = u % v;
@@ -36,6 +43,13 @@ void Fraction::reduce(void) {
     den /= u;
 }
 
+//Print method:
+//The rules for printing a fraction can be complex and this method takes
+//into account many scenarios and prints the fraction in the most sensible
+//way
+
+//For example: The fraction 3/-1 should be printed as -3 and not in fraction
+//form
 void Fraction::print(void) {
     
     if ( abs(num) < abs(den) ) {
@@ -47,11 +61,16 @@ void Fraction::print(void) {
             cout << num * -1;
         else if ( den > 0)
             cout << num << "/" << den;
-        else if ( den < 0)
+        else if ( den < 0) 
+	    //if the fraction has the form 3/-4 we want to print -3/4
             cout << num * -1 << "/" << den * -1;
     }
     
-    else if (den != 1 && den != -1) {
+    //The following case is a fraction that has numerator greater than its
+    //denominator (-5/3)
+    //We want to print it with the integer value followed by its fractional
+    //portion
+    else if (den != 1 && den != 1) {
         int coef = num / den;
         Fraction temp;
         if (coef > 0)
@@ -62,14 +81,19 @@ void Fraction::print(void) {
         temp.print();
         cout  << ")";
     }
-    
+   
+    //The following case is a fraction over 1 (5/1) or (-5/1)
+    //We simply print the numerator. 
     else if (den == 1)
         cout << num;
     
+    //A fraction over negative 1.
+    //We print the inverse of the numerator.
     else if (den == -1)
         cout << num * -1;
 }
 
+//Redefining the addition operator for 2 fraction objects
 Fraction Fraction::operator+ (const Fraction& param) {
     int v = den * param.den;
     int u = (v / den + num) + (v / param.den + param.num);
@@ -77,6 +101,7 @@ Fraction Fraction::operator+ (const Fraction& param) {
     return temp;
 }
 
+//Redefining the addition operator for a fraction object with an integer
 Fraction Fraction::operator+ (int param) {
     Fraction temp (num + param * den, den);
     return temp;
@@ -114,6 +139,8 @@ Fraction Fraction::operator/ (int param) {
     return temp;
 }
 
-int Fraction::getNum (void) { return this -> num; }
+//The following methods simply return the value of the fraction's
+//numerator and denominator
+int Fraction::getNum (void) { return num; }
 
-int Fraction::getDen (void) { return this -> den; }
+int Fraction::getDen (void) { return den; }
